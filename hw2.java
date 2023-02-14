ab68e9215b50c3ec746257df2b46d33d1cc106e9
AntonIsl <arshavin0511@gmail.com>
2023-02-14 11:32:02 +0300

    2ая задача готово

--- hw2.java
+++ hw2.java
@@ -1,1 +1,12 @@
 //Вывести все простые числа от 1 до 1000 
+public class hw2 {
+    
+    public static void main(String[] args) {
+        int num = 0;
+        for (int i = 0; i < 1000; i++) {
+            num++;
+            System.out.print(num + " ");
+        }
+
+    }
+}

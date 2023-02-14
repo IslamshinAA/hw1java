5e9ecd91748167a2726eb631b6b532eb5bdd0524
AntonIsl <arshavin0511@gmail.com>
2023-02-14 13:07:30 +0300

    3ая задача готово

--- hw1.java
+++ hw1.java
@@ -1,3 +1,27 @@
 // 1. Вычислить n-ое треугольного число(сумма чисел от 1 до n), n!
 // (произведение чисел от 1 до n)
 
+public class hw1 {
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+      int[] arr = new int[6]; // можно задать любое кол-во элементов
+      int[] newarr = new int[6]; // массив для полученых чисел от одного до n
+      int temp = 0;
+      for (int i = 1; i < arr.length; i++) { //1, 3, 6, 10, 15
+        arr[i] += temp;
+        temp ++;
+        arr[i] = temp + arr[i-1];
+        newarr[i] = arr[i];
+      }   
+      int res = 1;
+      int fini = 0;
+      for (int i = 1; i < newarr.length; i++) {
+       res = res * newarr[i];
+       fini = res;// 2700
+      }
+      System.out.print(fini); 
+    }
+
+ }
